1c062da51201fd834a00ce19e729abe4ebf7923c
yang-lel <951730676@qq.com>
2021-01-25 10:18:24 +0800

    logs

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" @click="clickimg">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  methods:{
+    clickimg(){
+      console.log(this);
+    }
+  }
+}
+</script>
